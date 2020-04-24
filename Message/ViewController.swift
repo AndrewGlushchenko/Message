c2cbc898effe33e6fe82b78d8f08ee404921205f
Andrew Glushchenko <aglushchenko@me.com>
2020-04-24 22:01:57 +0300

    Send a message from the application

--- Message/ViewController.swift
+++ Message/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import MessageUI
 
 class ViewController: UIViewController {
 
+    
+    var messageController = MFMessageComposeViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pushSendMessageAction(_ sender: Any) {
+        if MFMessageComposeViewController.canSendText() {
+            print("canSendText")
+            return
+        }
+        messageController.recipients = ["+79161066139", "g4andrey@gmail.com"]
+        messageController.body = "Не хочу быть телефоном, хочу быть тамогочей"
+        
+        messageController.messageComposeDelegate = self
+        
+        present(messageController, animated: true, completion: nil)
+    }
+    
+}
+
+
+extension ViewController: MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+        
+        switch result {
+        case MessageComposeResult.cancelled:
+            print("Message cancelled")
+        case MessageComposeResult.sent:
+            print("Message sent")
+        case MessageComposeResult.failed:
+            print("Message failed")
+        default:
+            print("Default Action")
+        }
+        
+        
+    }
 
 }
 
