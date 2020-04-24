b5d0399707a07fe992786d486d583e6b41844ccc
Andrew Glushchenko <aglushchenko@me.com>
2020-04-24 21:08:32 +0300

    Initial Commit

---
+++ Message/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Message
+//
+//  Created by gandrey on 24/04/2020.
+//  Copyright © 2020 GlAndrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
